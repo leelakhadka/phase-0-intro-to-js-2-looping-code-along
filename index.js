6efff38a41e2789744ada04c6330856bfad53703
leela khadka <khadkaleela3@gmail.com>
2022-07-23 17:07:10 -0700

    complted tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(gifts, surprise) {
+    const newArray = [];
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`);
+        newArray.push(`Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown(number) {
+    for (let i = 0; i <= number; i++) {
+        console.log(i);
+    }
+}
